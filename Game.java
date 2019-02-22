ea95a158c66c4d10fa079fddd460aa440ded0cb0
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-02-21 21:44:11 -0500

    add turn prompt logic

--- Game.java
+++ Game.java
@@ -11,7 +11,7 @@ public class Game {
 		printTitle();
 		String response = titlePrompt();
 
-		if (response == "start") {
+		if (response.contentEquals("start")) {
 			gameLoop();
 		}
 
@@ -20,7 +20,12 @@ public class Game {
 
 	// The main loop of the game.
 	private void gameLoop() {
-		// TOOD
+		String response = "";
+		while (!response.contentEquals("exit")) {
+			// TODO: Display game board.
+			response = turnPrompt();
+			// TODO: Handle validated user input.
+		}
 	}
 
 	// Display the text-art title of the game.
@@ -32,22 +37,22 @@ public class Game {
 		System.out.println("#   #  #####  #   #  #####  ####    # #   #####  #####  #      #####  #   #\n");
 	}
 
-	// Prompt user for whether they would like to start a game of Minesweeper, 
+	// Prompt user for whether they would like to start a game of Minesweeper,
 	// exit, or display the help menu
 	private String titlePrompt() {
 		Scanner input = new Scanner(System.in);
 
 		String response = "";
-		while (!response.equals("start") && !response.equals("exit")) {
+		while (!response.contentEquals("start") && !response.contentEquals("exit")) {
 			System.out.println("Enter 'start' to start a new game, 'exit' to exit, or 'help' to list in-game controls.");
 			System.out.print("> ");
 			response = input.nextLine();
 
-			boolean valid = response.equals("start") || response.equals("exit") || response.equals("help");
+			boolean valid = response.contentEquals("start") || response.contentEquals("exit") || response.contentEquals("help");
 			if (!valid) {
-				System.err.println("Please enter a valid option.\n");
+				System.out.println("Please enter a valid option.\n");
 			}
-			else if (response.equals("help")) {
+			else if (response.contentEquals("help")) {
 				printHelp();
 			}
 		}
@@ -57,11 +62,52 @@ public class Game {
 
 	// Display a list of valid inputs to the user.
 	private void printHelp() {
-		// TODO
+		// TODO: Write actual help menu.
+		System.out.println("\nIf the help menu were written yet, this is where it would go.\n");
 	}
 
 	// Display a message on program exit thanking the user for playing the game.
 	private void printProgramExit() {
 		System.out.println("\nThank you for playing!");
+	}
+
+	// Ask the user for an input and validate it. Returns the user's move for
+	// the turn as a string.
+	private String turnPrompt() {
+		String response = "";
+
+		Scanner input = new Scanner(System.in);
+		boolean valid = false;
+		while (!valid || response.contentEquals("help")) {
+			System.out.print("> ");
+			response = input.nextLine();
+
+			if (response.contentEquals("exit")) {
+				valid = true;
+			}
+			else if (response.contentEquals("help")) {
+				valid = true;
+				printHelp();
+			}
+			else if (isTile(response)) {
+				valid = true;
+			}
+			else {
+				valid = false;
+				System.out.println("Input not valid! Enter 'help' if needed.\n");
+			}
+		}
+
+		return response;
+	}
+
+	// Takes a String and returns true if the content is a tile on the
+	// board (A7) or an exclamation point followed by a tile (!B3).
+	private boolean isTile(String str) {
+		boolean isTile = false;
+
+		// TODO: Write body of method.
+
+		return isTile;
 	}
 }

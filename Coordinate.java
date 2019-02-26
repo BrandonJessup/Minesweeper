d76e0ab6cb04935465e158df12f0bf58289fb081
Brandon Jessup <brandonwilliamsjessup@gmail.com>
2019-02-26 18:45:49 -0500

    Add Coordinate class

---
+++ Coordinate.java
@@ -0,0 +1,18 @@
+/*
+ * A simple container for an x and y value.
+ */
+
+public class Coordinate {
+	public int x;
+	public int y;
+
+	public Coordinate() {
+		x = 0;
+		y = 0;
+	}
+
+	public Coordinate(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
